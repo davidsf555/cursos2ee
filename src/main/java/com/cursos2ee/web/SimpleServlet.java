93d16626631e6d5285041e86df18924e8567e159
DavidSF <d.sanchez@extern.isban.com>
2017-05-13 11:03:00 +0200

    first servlet

---
+++ src/main/java/com/cursos2ee/web/SimpleServlet.java
@@ -0,0 +1,27 @@
+package com.cursos2ee.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by usuario on 13/05/2017.
+ */
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Mi primer servlet chispas");
+        out.flush();
+        out.close();
+    }
+}
